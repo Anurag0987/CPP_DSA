4961794a24e673870468d5218e691b4e82d244ed
Anurag Mangalampalli <AnuragM@winjittechnologiespvtltd.local>
2026-03-20 00:00:34 +0530

    Pattern 2 and 3 - AM

--- star_patterns.cpp
+++ star_patterns.cpp
@@ -10,9 +10,29 @@ void pattern1(int n){
     }
 }
 
+void pattern2(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+void pattern3(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << j+1;
+        }
+        cout << "\n";
+    }
+}
+
 int main(){
     int n;
     cin >> n;
-    pattern1(n);
+    // pattern1(n);
+    // pattern2(n);
+    pattern3(n);
     return 0;
 }
