406fec59894ab410815e60fd17c1e06a756f1946
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-03-21 17:20:35 +0530

    Pattern 6 and 7 - AM

--- star_patterns.cpp
+++ star_patterns.cpp
@@ -46,6 +46,34 @@ void pattern5(int n){
     }
 }
 
+void pattern6(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout << j+1;
+        }
+        cout << "\n";
+    }
+}
+
+void pattern7(int n){
+    // The code can be made shorter and cleaner, but the time complexitity remain same as any other solution for this, i.e., O(n2)
+    for(int i=1; i<=n; i++){
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < 2*i-1; k++)
+        {
+            cout << "*";
+        }
+        for (int l = 0; l < n-i; l++)
+        {
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main(){
     int n;
     cin >> n;
@@ -53,6 +81,8 @@ int main(){
     // pattern2(n);
     // pattern3(n);
     // pattern4(n);
-    pattern5(n);
+    // pattern5(n);
+    // pattern6(n);
+    pattern7(n);
     return 0;
 }
