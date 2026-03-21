12c62c577d1b32645c2bbb755f73192f606798d6
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-03-21 16:26:22 +0530

    Pattern 4 and 5 - AM

--- star_patterns.cpp
+++ star_patterns.cpp
@@ -28,11 +28,31 @@ void pattern3(int n){
     }
 }
 
+void pattern4(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout << i+1;
+        }
+        cout << "\n";
+    }
+}
+
+void pattern5(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout << '*';
+        }
+        cout << "\n";
+    }
+}
+
 int main(){
     int n;
     cin >> n;
     // pattern1(n);
     // pattern2(n);
-    pattern3(n);
+    // pattern3(n);
+    // pattern4(n);
+    pattern5(n);
     return 0;
 }
