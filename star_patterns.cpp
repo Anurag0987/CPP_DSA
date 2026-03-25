80ee0321180abbd48acdaca88776ce102ec53826
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-03-25 23:29:53 +0530

    Pattern 21 - AM

--- star_patterns.cpp
+++ star_patterns.cpp
@@ -460,6 +460,43 @@ void pattern20(int n)
     }
 }
 
+void pattern21(int n){
+    for (int i = 0; i < n; i++)
+    {
+        if(i == 0 || i == n-1){
+            for (int j = 0; j < n-1; j++)
+            {
+                cout << "*";
+            }
+        }
+        else
+        {
+            cout << "*";
+            for (int j = 0; j < n-2; j++)
+            {
+                cout << " ";
+            }
+        }
+        cout << "*";
+        cout << "\n";
+    }
+}
+// OR
+void pattern21_2(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i == 0 || i == n-1 || j == 0 || j == n-1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
 int main()
 {
     int n;
@@ -487,6 +524,8 @@ int main()
     // pattern17_2(n);
     // pattern18(n);
     // pattern19(n);
-    pattern20(n);
+    // pattern20(n);
+    // pattern21(n);
+    pattern21_2(n);
     return 0;
 }
