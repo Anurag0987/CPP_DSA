08eee2e664948f83c95f54862891e5268ce10d98
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-04-02 23:51:27 +0530

    Pointer_0.1 - AM

---
+++ STL_2.1_Pointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Any Variable in C++ is lives in memory
+    int x = 100;
+    cout << "value of X:" << x << "\n";
+    cout << "Address of X:" << &x << "\n";
+    // Pointer = Variable that stores address of another variable.
+}
