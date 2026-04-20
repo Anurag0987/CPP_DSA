9eb87a953a2f237712802c3b07d5264d94e9966d
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-04-20 22:48:17 +0530

    Small corrections - AM

--- STL_2.1_Pointer.cpp
+++ STL_2.1_Pointer.cpp
@@ -5,7 +5,7 @@ int main(){
     // Any Variable in C++ is lives in memory
     int x = 100;
     cout << "value of x: " << x << "\n";
-    cout << "Address of x: " << &x << "\n";
+    cout << "Address of &x: " << &x << "\n";
 
     // Pointer = Variable that stores address of another variable.
     int *p = &x; // <-- '&' is used for "Pass by reference"
@@ -13,12 +13,12 @@ int main(){
     // *p means go to address of p and get the value
 
     cout << "Addess of p: "<< p << "\n";
-    cout << "value of p: "<< *p << "\n";
+    cout << "value of *p: "<< *p << "\n";
 
     cout << "Changing the value of p to verify the effect of x;" << "\n";
     *p = 10;
     cout << "New value of x: " << x << "\n";
-    cout << "New value of p: " << *p << "\n";
+    cout << "New value of *p: " << *p << "\n";
 
     int y = 10;
 
@@ -26,8 +26,8 @@ int main(){
     int &r = y;    // reference
 
     cout << "value of p1: " << p1 << "\n";
-    cout << "value of p1: " << *p1 << "\n";
+    cout << "value of *p1: " << *p1 << "\n";
 
     cout << "value of r: " << r << "\n";
-    cout << "value of r: " << &r << "\n";
+    cout << "value of &r: " << &r << "\n";
 }
