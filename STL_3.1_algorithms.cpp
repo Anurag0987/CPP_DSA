83d6e013896ffbfd09fd9bb7c3f9e2a6027306a9
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-05-10 00:00:13 +0530

    Algorithm 1.2 sort explain- AM

--- STL_3.1_algorithms.cpp
+++ STL_3.1_algorithms.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 void exploreSort(){
-    
+    // CPP offers the .sort() function which is most efficinet for most of the cases
+    // i.e,. no need to implement custo sorting techniques like Heap sort, Bubble sort etc.. for generic cases
+    // .sort() accepts 2 arguments by default, .sort(a, a+n)
+    // a -> Starting of Index in container - initial iterator 
+    // a+n -> the Last Index in container, end iterator which is end+1
+    // [start, end)
+    // Can proved specific range in container to get sorted rather tahn full container.
+
 }
 
 int main (){
