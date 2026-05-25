d97fc971d01b279f1f9db3c8502d3abcafd40504
Anurag Mangalampalli <anuragmangalampalli144@gmail.com>
2026-05-25 23:57:05 +0530

    Recursion backtracking 1.1 -AM

---
+++ recursion_backtracking.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h> //<-- this one library includes all essential libraries like math.h, stringh.h, <utility>
+using namespace std;
+
+
+void recursionBackTracking1toN(int n){
+    if(n < 1) return;
+    recursionBackTracking1toN(n-1);
+    cout << "Value of N - " << n << endl; // <-- 1st Print is executed at last
+    // ex: initial n value = 4, then n-1 => 3, then n-1 => 2, then n-1 => 1, then n-1 => 0
+    // But the print order is 1, 2, 3, 4
+    // Because the cout is called after the fn() call, so last function line will be execture first 
+    // and return's to last-1 function which executes it's cout and return's last-2 so on..
+    // This is called ****Backtracking****
+}
+
+int main(){
+    // Problem Statement - Print 1 - N using recursion and *Backtracking*
+    int n;
+    cin >> n;
+
+    recursionBackTracking1toN(n);
+}
